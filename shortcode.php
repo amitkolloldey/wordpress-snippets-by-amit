9911cc29276a7237e5d937f1368c2d666748f8a9
Amit Kollol Dey <amitkolloldey@gmail.com>
2017-03-11 19:19:09 +0600

    Update shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -24,3 +24,30 @@ function amit_shortcode_function( $atts ) {
 
 }
 add_shortcode( 'amit_shortcode', 'amit_shortcode_function' );
+
+
+// Add Double Shortcode
+function amit_shortcode_function_double( $atts , $content = null ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'src' => '',
+			'width' => '',
+			'height' => '',
+		),
+		$atts,
+		'amit_shortcode_double'
+	);
+
+	// Return custom embed code
+	return '<embed 
+	         src="' . $atts['src'] . '"
+	         width="' . $atts['width'] . '"
+	         height="' . $atts['height'] . '"
+	         type="application/x-shockwave-flash"
+	         allowscriptaccess="always"
+	         allowfullscreen="true">';
+
+}
+add_shortcode( 'amit_shortcode_double', 'amit_shortcode_function_double' );
